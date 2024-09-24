0a09ab944a3eee191cc642e6207c0c26a12eb956
wushucan <wushucan@wps.cn>
2024-09-24 14:31:43 +0800

    连接数据库

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 	"unicode/utf8"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var router = mux.NewRouter()
+var db *sql.DB
 
 type ArticlesFormData struct {
 	Title  string
@@ -129,7 +132,39 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initDB() {
+	var err error
+	config := mysql.Config{
+		User:                 "root",
+		Passwd:               "wsc963..",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "goblog",
+		AllowNativePasswords: true,
+	}
+	db, err = sql.Open("mysql", config.FormatDSN())
+	checkError(err)
+
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	err = db.Ping()
+	checkError(err)
+}
+
 func main() {
+
+	initDB()
 
 	router.HandleFunc("/", defaultFunc).Methods("GET").Name("default")
 	router.HandleFunc("/about", aboutFunc).Methods("GET").Name("about")
