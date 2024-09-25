985f3c12eb1a33fb69aac360010ea512504ea7a9
wushucan <wushucan@wps.cn>
2024-09-25 12:45:33 +0800

    精简main.go

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"github.com/gorilla/mux"
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"goblog/pkg/logger"
 	"net/http"
 )
 
-var router = mux.NewRouter()
-var db *sql.DB
-
 func main() {
-
-	database.InitDB()
-	db = database.DB
-
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
 	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 	logger.LogError(err)
